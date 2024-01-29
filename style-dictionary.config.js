7828e93f6d7d30cb344565e4fceb851e7bb9e2c2
labri70 <labri70@naver.com>
2024-01-29 19:07:38 +0900

    내용재수정1

--- style-dictionary.config.js
+++ style-dictionary.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  source: ['./build/tokens.json'], // Use the same output path as in token-transformer.config.js
+  source: ['./theme/tokens.json'], // Use the same output path as in token-transformer.config.js
   platforms: {
     // Define your style dictionary platforms (e.g., 'css', 'scss', 'android', 'ios', etc.)
     css: {
