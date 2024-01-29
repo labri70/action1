80884419fed11e01c245af5d777fceeca4ee0a5c
labri70 <labri70@naver.com>
2024-01-29 17:47:09 +0900

    Delete processTokens.js

--- processTokens.js
+++
@@ -1,7 +0,0 @@
-const { execSync } = require('child_process');
-
-// Run token-transformer and style-dictionary
-execSync('npx token-transformer -c token-transformer.config.js');
-execSync('npx style-dictionary build -c style-dictionary.config.js');
-
-console.log('Figma tokens processed successfully!');
